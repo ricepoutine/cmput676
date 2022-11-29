bb9a18e9567fe20eac8daa39a8479e6705888058
ricepoutine <dutx99@gmail.com>
2022-11-28 19:42:26 -0700

    need to test feature extraction

---
+++ helper.py
@@ -0,0 +1,6 @@
+##### HELPER FUNCTION FOR FEATURE EXTRACTION: https://github.com/kozodoi/website/blob/master/_notebooks/2021-05-27-extracting-features.ipynb
+
+def get_features(name, dict): #dict is dictionary for saving features, should be called "features"
+    def hook(model, input, output):
+        dict[name] = output.detach()
+    return hook
